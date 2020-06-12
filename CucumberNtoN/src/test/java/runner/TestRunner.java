d3546a53fe9b28307b9e52c1b324f30576b517fd
khaled <khaled.mahmud199@gmail.com>
2020-06-12 13:30:42 -0400

    update 06/12

---
+++ CucumberNtoN/src/test/java/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions
+         (
+        	features=".//Features/UpsRefrence.feature",	
+        	glue = {"stepDefinition"}      	      	
+        	     
+        		 
+         )
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+}
+
+
